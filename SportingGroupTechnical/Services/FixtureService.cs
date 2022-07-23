64eae8239c8c17dc597798fcf8af3eb37c41c814
Jack Butcher <jack_butcher17@hotmail.co.uk>
2022-07-23 11:10:00 +0100

    Add FixtureService class

---
+++ SportingGroupTechnical/Services/FixtureService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportingGroupTechnical.Models;
+
+namespace SportingGroupTechnical.Services
+{
+    /*
+     * Fixture Service
+     * ---------------
+     * A simple in-memory service for storing Fixtures, supporting get all, get by ID, and add.
+     */
+    public static class FixtureService
+    {
+        private static readonly List<Fixture> mFixtures = new List<Fixture>();
+        private static Int32 mNextId = 1;
+
+        /*
+         * Get All
+         * -------
+         * Returns the List of Fixtures.
+         */
+        public static List<Fixture> GetAll() => mFixtures;
+
+        /*
+         * Get
+         * ---
+         * Returns the Fixture in the List with the given ID, or null if it doesn't exist.
+         */
+        public static Fixture? Get(Int32 id) => mFixtures.FirstOrDefault(f => f.Id == id);
+
+        /*
+         * Add
+         * ---
+         * Adds the given Fixture to the List with the next ID, which is then incremented.
+         * Returns the ID of the Fixture.
+         */
+        public static Int32 Add(Fixture fixture)
+        {
+            fixture.Id = mNextId++;
+            mFixtures.Add(fixture);
+            return fixture.Id;
+        }
+    }
+}
