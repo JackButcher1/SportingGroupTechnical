6b18461b90febb416a2533e6f0bf72ecd1278b49
Jack Butcher <jack_butcher17@hotmail.co.uk>
2022-07-23 11:48:26 +0100

    Add team name properties to Fixture class

--- SportingGroupTechnical/Models/Fixture.cs
+++ SportingGroupTechnical/Models/Fixture.cs
@@ -11,6 +11,8 @@ namespace SportingGroupTechnical.Models
     public class Fixture
     {
         public Int32 Id { get; set; }                           // Unique fixture ID.
+        public String HomeTeam { get; set; }                    // Home team name.
+        public String AwayTeam { get; set; }                    // Away team name.
         public DateTime Start { get; set; }                     // Start date & time.
         public Result Result { get; set; }                      // Fixture result. Updated when the fixture is either completed or cancelled.
         public Dictionary<Result, Decimal> Odds { get; set; }   // Dictionary defining decimal odds for each result.
