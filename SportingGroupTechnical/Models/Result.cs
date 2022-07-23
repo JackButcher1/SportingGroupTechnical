a379b788b9ccf8af2d2532d22006eaa952700667
Jack Butcher <jack_butcher17@hotmail.co.uk>
2022-07-23 11:46:53 +0100

    Add String/enum JSON converter attribute to Result enum to allow writing enums as strings in JSON

--- SportingGroupTechnical/Models/Result.cs
+++ SportingGroupTechnical/Models/Result.cs
@@ -1,10 +1,14 @@
-﻿namespace SportingGroupTechnical.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SportingGroupTechnical.Models
 {
     /*
      * Result
      * ------
      * Defines the result of a fixture.
+     * The JsonConverter attribute allows us to send/receive HTTP requests/replies with the Result property written as a String.
      */
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Result
     {
         UNDECIDED,
