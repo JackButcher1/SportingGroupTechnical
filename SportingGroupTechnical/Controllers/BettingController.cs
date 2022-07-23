fcd2c8ae8e2949a5ce7edd9b65238868f5f6350a
Jack Butcher <jack_butcher17@hotmail.co.uk>
2022-07-23 12:27:26 +0100

    Add BettingController to route HTTP requests to BettingService

---
+++ SportingGroupTechnical/Controllers/BettingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using SportingGroupTechnical.Models;
+using SportingGroupTechnical.Services;
+
+namespace SportingGroupTechnical.Controllers
+{
+    /*
+     * Betting Controller
+     * ------------------
+     * Controls HTTP requests for the Betting Service, implementing the following endpoints:
+     *  - GET /bet/{id}     (gets bet with ID={id})
+     *  - POST /bet         (adds a new bet)
+     */
+    [ApiController]
+    [Route("bet")]
+    public class BettingController : ControllerBase
+    {
+        /*
+         * Get
+         * ---
+         * HTTP GET requests with a specified ID are routed here. Returns the Betting Service's Get method,
+         * or NotFound if null.
+         */
+        [HttpGet("{id}")]
+        public ActionResult<Bet> Get(Int32 id)
+        {
+            Bet? bet = BettingService.Get(id);
+            if (bet is null)
+            {
+                return NotFound();
+            }
+            return bet;
+        }
+
+        /*
+         * Create
+         * ------
+         * HTTP POST requests are routed here. Passes the given Bet into the Betting Service's Add method,
+         * then returns an HTTP created response.
+         */
+        [HttpPost]
+        public IActionResult Create(Bet bet)
+        {
+            BettingService.Add(bet);
+            return CreatedAtAction(nameof(Create), new { id = bet.Id }, bet);
+        }
+    }
+}
